22e07474922ce39f08e8115def855f15fa34ae97
9demnex <stropsdev@gmail.com>
2024-05-30 11:33:32 -0400

    Build 1.0.0 - 053024

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+document.querySelectorAll('.color-scheme-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        document.documentElement.setAttribute('data-scheme', this.dataset.scheme);
+    });
+});
